26cbb8f6b1cca47c197e6af61ccb7bdfdac3edca
Adam Betts <abetts@doc.ic.ac.uk>
2011-07-15 13:57:00 +0100

    Added source file

---
+++ src/adam/betts/tools/MainProgramAnalyser.java
@@ -0,0 +1,115 @@
+package adam.betts.tools;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import adam.betts.programs.Program;
+import adam.betts.tools.MainTraceGenerator.Globals;
+import adam.betts.utilities.Debug;
+import adam.betts.utilities.DefaultOptions;
+
+public class MainProgramAnalyser
+{
+	private static Options options;
+	private static Option inlineOption;
+	private static Option loopsOption;
+	private static Option syntaxTreesOption;
+
+	private static void addOptions ()
+	{
+		options = new Options ();
+		DefaultOptions.addDefaultOptions (options);
+		DefaultOptions.addProgramOption (options);
+		DefaultOptions.addRootOption (options);
+		DefaultOptions.addUDrawDirectoryOption (options);
+
+		inlineOption = new Option ("I", "inline", false,
+				"Inline all control flow graphs to create a single monolothic graph.");
+		inlineOption.setRequired (false);
+		options.addOption (inlineOption);
+
+		loopsOption = new Option ("l", "loops", false,
+				"Generate the loop-nesting trees in the program.");
+		loopsOption.setRequired (false);
+		options.addOption (loopsOption);
+
+		syntaxTreesOption = new Option ("s", "syntax-trees", false,
+				"Generate the syntax trees of the control flow graphs.");
+		syntaxTreesOption.setRequired (false);
+		options.addOption (syntaxTreesOption);
+	}
+
+	private static void parseCommandLine (String[] args)
+	{
+		final String toolName = "program-analyser.jar";
+		CommandLineParser parser = new GnuParser ();
+		HelpFormatter formatter = new HelpFormatter ();
+		formatter.setWidth (128);
+		CommandLine line = null;
+		try
+		{
+			line = parser.parse (options, args);
+
+			if (line.hasOption (DefaultOptions.helpOption.getOpt ()))
+			{
+				formatter.printHelp (toolName, options);
+				System.exit (1);
+			} else
+			{
+				DefaultOptions.setDefaultOptions (line);
+				DefaultOptions.setProgramOption (line);
+				DefaultOptions.setRootOption (line);
+				DefaultOptions.setUDrawDirectoryOption (line);
+
+				Globals.inline = line.hasOption (inlineOption.getOpt ());
+
+				Globals.LNTs = line.hasOption (loopsOption.getOpt ());
+
+				Globals.ASTs = line.hasOption (syntaxTreesOption.getOpt ());
+			}
+		} catch (ParseException e)
+		{
+			System.out.println (e.getMessage ());
+			formatter.printHelp (toolName, options);
+			System.exit (1);
+		}
+	}
+
+	private static void run ()
+	{
+		Debug.verboseMessage ("Reading program");
+
+		Program program = new Program ();
+
+		if (Globals.LNTs)
+		{
+			Debug.verboseMessage ("Building loop-nesting trees");
+			program.buildLNTs ();
+		}
+
+		if (Globals.ASTs)
+		{
+			Debug.verboseMessage ("Building abstract syntax trees");
+			program.buildSyntaxTrees ();
+		}
+	}
+
+	public static void main (String[] args)
+	{
+		addOptions ();
+		parseCommandLine (args);
+		run ();
+	}
+
+	public static class Globals
+	{
+		protected static boolean inline;
+		protected static boolean LNTs;
+		protected static boolean ASTs;
+	}
+}
