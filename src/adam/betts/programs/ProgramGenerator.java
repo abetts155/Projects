7bb5e7354d1520cd47c71edecdb1129e433ef592
Roxana Danila <id210@ic.ac.uk>
2011-07-21 10:55:57 +0100

    wird call graph

--- src/adam/betts/programs/ProgramGenerator.java
+++ src/adam/betts/programs/ProgramGenerator.java
@@ -1,8 +1,12 @@
 package adam.betts.programs;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.ListIterator;
 import java.util.Random;
 
+import adam.betts.edges.Edge;
 import adam.betts.graphs.CFGGenerator;
 import adam.betts.graphs.ControlFlowGraph;
 import adam.betts.outputs.OutputGraph;
@@ -17,6 +21,8 @@ public class ProgramGenerator
 {
 	protected ArrayList<Subprogram> subprograms = new ArrayList<Subprogram> ();
 	protected Program program = new Program ();
+	protected ArrayList<Subprogram> levelSet = new ArrayList<Subprogram> ();
+	protected HashMap<Integer, ArrayList<Subprogram>> levelMap = new HashMap<Integer, ArrayList<Subprogram>> ();
 	protected CallGraph callgraph = new CallGraph ();
 	protected ControlFlowGraph root;
 	protected Random gen = new Random();
@@ -83,20 +89,23 @@ public class ProgramGenerator
 	
 	private void add () 
 	{
-		int level = 1;
+		int currentLevel = 1;
 		int subprogramsLeft = numOfSubprograms - 1;
+		Subprogram currentSubprogram = program.getSubprogram(1);
 	
 		for (int i = 1; i < numOfSubprograms; ++i)
 		{
-			if (level < depth)
+			if (currentLevel < depth)
 			{	
-				Subprogram s = program.getSubprogram (i);						
-				s.getCFG().setSubprogramName (s.getSubprogramName());
-				
 				if (subprogramsLeft > 0)
 				{
-					int numOfSuccessors = gen.nextInt (subprogramsLeft);
-					int successorID = gen.nextInt (numOfSubprograms - i + 1) + i;	
+					Subprogram s = program.getSubprogram (i);						
+					s.getCFG().setSubprogramName (s.getSubprogramName());
+					
+					int successorID = gen.nextInt (numOfSubprograms - i + 1) + i;
+					//System.out.println ("successor id " + i + " " + successorID);
+					//levelSet = new ArrayList<Subprogram>();
+					
 					while (successorID > i)
 					{			
 						Subprogram sNext = program.getSubprogram (successorID);	
@@ -105,34 +114,72 @@ public class ProgramGenerator
 						
 						callgraph.addCall (s.getSubprogramName (), sNext.getSubprogramName (), i);
 						Debug.debugMessage (getClass (), "Adding call " + Integer.toString (i), 4);
-						numOfSuccessors--;
-						
+						//levelSet.add (sNext);
+												
 						if (!subprograms.contains (sNext))
 						{
 							subprogramsLeft--;
 							subprograms.add (sNext);
+							currentSubprogram = sNext;
 						}	
 					}
-					level++;
+					//levelMap.put (currentLevel, levelSet);
+					currentLevel++;
 				}
+				/*else 
+				{
+					i = numOfSubprograms + 1;
+					
+				}*/
 			}	
 			else
+			{	
 				i = numOfSubprograms + 1;
-		}	
+			}	
 			
+		}	
+		
+		if (subprogramsLeft > 0)
+		
 		for (int i = 2; i <= numOfSubprograms; ++i)
 		{
 			Subprogram s = program.getSubprogram (i);
 			if (!subprograms.contains (s))
 			{
+				subprograms.add (s);
 				int subprogramID = gen.nextInt(numOfSubprograms - i + 1) + i;
-				Subprogram subprogram = program.getSubprogram (subprogramID);
+				Subprogram subprogram = program.getSubprogram (subprogramID); 
 				if (!callgraph.isLeaf (subprogramID))
 					callgraph.addCall (subprogram.getSubprogramName (), s.getSubprogramName (), i);
 				else
 					callgraph.addCall (root.getSubprogramName (), s.getSubprogramName (), i);
 				Debug.debugMessage (getClass (), "Adding call " + Integer.toString(i), 4);	
 			}
+		}
+		
+		System.out.println ("current level: " + currentLevel);
+		
+		if (currentLevel < depth)
+		{
+			int levelsToBeAdded = depth - currentLevel;			
+			for (Subprogram s : subprograms)
+				if (levelsToBeAdded > 0)
+				{	
+					if (callgraph.isLeaf (s.getSubprogramID ()))
+					{
+						callgraph.removeAllPredecessorEdges ();
+						callgraph.addCall(currentSubprogram.getSubprogramName (), s.getSubprogramName (), numOfSubprograms);
+						Debug.debugMessage (getClass (), "Adding call " + Integer.toString (numOfSubprograms), 4);	
+						currentSubprogram = s;
+						levelsToBeAdded--;
+						currentLevel++;
+					}
+				}	
+				else
+				{	
+					break;
+				}	
+			System.out.println ("levels: " + currentLevel);
 		}
 	}
 }
