4762d06a4a5f227fdc5e4a7aa5640990137725d8
Adam Betts <abetts@imperial.ac.uk>
2012-06-08 12:57:15 +0100

    Added new scripts to control CFG and PST generation

---
+++ src/adam/betts/tools/MainProgramSyntaxTreeAnalysis.java
@@ -0,0 +1,77 @@
+package adam.betts.tools;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import adam.betts.programs.Program;
+import adam.betts.utilities.Debug;
+import adam.betts.utilities.DefaultOptions;
+
+public class MainProgramSyntaxTreeAnalysis
+{
+
+    private static Options options;
+
+    private static String programFileName;
+
+    private static void addOptions ()
+    {
+        options = new Options();
+        DefaultOptions.addDefaultOptions(options);
+        DefaultOptions.addProgramOption(options);
+        DefaultOptions.addRootOption(options, false);
+        DefaultOptions.addUDrawDirectoryOption(options);
+    }
+
+    private static void parseCommandLine (String[] args)
+    {
+        final String toolName = "pst.jar";
+        CommandLineParser parser = new GnuParser();
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.setWidth(128);
+        CommandLine line = null;
+        try
+        {
+            line = parser.parse(options, args);
+
+            if (line.hasOption(DefaultOptions.helpOption.getOpt()))
+            {
+                formatter.printHelp(toolName, options);
+                System.exit(1);
+            }
+            else
+            {
+                DefaultOptions.setDefaultOptions(line);
+                DefaultOptions.setRootOption(line);
+                DefaultOptions.setUDrawDirectoryOption(line);
+
+                programFileName = line
+                        .getOptionValue(DefaultOptions.programFileOption
+                                .getOpt());
+            }
+        }
+        catch (ParseException e)
+        {
+            System.out.println(e.getMessage());
+            formatter.printHelp(toolName, options);
+            System.exit(1);
+        }
+    }
+
+    private static void run ()
+    {
+        Program program = new Program(programFileName);
+        program.buildSyntaxTrees();
+    }
+
+    public static void main (String[] args)
+    {
+        addOptions();
+        parseCommandLine(args);
+        run();
+    }
+}
