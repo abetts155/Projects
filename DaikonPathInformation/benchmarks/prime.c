46c81730ae7c5b657c54501311769e7110c052cc
Paul Thomson <paul.thomson11@imperial.ac.uk>
2013-09-17 18:19:52 +0100

    Instrumented prime.c

--- DaikonPathInformation/benchmarks/prime.c
+++ DaikonPathInformation/benchmarks/prime.c
@@ -21,20 +21,43 @@ even (unsigned int n)
 unsigned char
 prime (unsigned int n)
 {
+  #ifdef CBMC
+  int __count_3_11 = 0;
+  int __count_7_11 = 0;
+  int __count_8_9 = 0;
+  int __count_9_10 = 0;
+  int __count_L9 = 0;
+  #endif
   int i;
 
   if (even (n))
   {
+    #ifdef CBMC
+    __count_3_11++;
+    #endif
     return n == 2;
   }
 
-  for (i = 3; i * i <= n; i += 2)
+  for (i = 3; 
+    #ifdef CBMC
+    __count_L9++,
+    #endif
+    i * i <= n; i += 2)
   {
     if (divides (i, n))
     {
+      #ifdef CBMC
+      __count_7_11++;
+      #endif
       return 0;
     }
+    #ifdef CBMC
+    __count_8_9++;
+    #endif
   }
+  #ifdef CBMC
+  __count_9_10++;
+  #endif
   return n > 1;
 }
 
