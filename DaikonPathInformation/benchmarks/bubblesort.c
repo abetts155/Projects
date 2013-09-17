f6ef2017c3d3c378834deb03e160ae04698f351a
Adam Betts <a.betts@imperial.ac.uk>
2013-09-17 14:53:14 +0100

    Added instrumentation to bubblesort

--- DaikonPathInformation/benchmarks/bubblesort.c
+++ DaikonPathInformation/benchmarks/bubblesort.c
@@ -14,14 +14,25 @@
 int
 swapIfLarger (int *a, int *b)
 {
+  #ifdef CBMC
+  int __count_14_16 = 0;
+  int __count_15_17 = 0;
+  #endif
+
   int tmp;
   if (*a > *b)
   {	
+    #ifdef CBMC
+    __count_15_17++;
+    #endif
     tmp = *a;
     *a = *b;
     *b = tmp;
     return 1;
   }
+  #ifdef CBMC
+  else __count_14_16++;
+  #endif
   return 0;
 }
 
@@ -29,39 +40,54 @@ void
 bubblesort (int ARRAY_SIZE, int a[])
 {
   #ifdef CBMC
-  int __countL1 = 0;
-  int __countL2 = 0;
+  int __count_L10 = 0;
+  int __count_L7 = 0;
+  int __count_5_6 = 0;
+  int __count_8_9 = 0;
+  int __count_12_13 = 0;
+  int __count_4_6 = 0;
+  int __count_11_13 = 0;
   #endif
 
   int i, j, tmp;
   int swapped = 0;
-  for (i = 0; i < ARRAY_SIZE - 1; i++)
+  for (i = 0;
+       #ifdef CBMC
+       __count_L10++,
+       #endif
+       i < ARRAY_SIZE - 1;
+       i++)
   {
-    #ifdef CBMC
-    __countL1++;
-    #endif
-    
     swapped = 0;
-    for (j = 0; j < ARRAY_SIZE - 1 - i; j++)
+    for (j = 0; 
+         #ifdef CBMC
+         __count_L7++,
+         #endif
+         j < ARRAY_SIZE - 1 - i; 
+         j++)
     {
-      #ifdef CBMC
-      __countL1++;
-      #endif
-      
       if (swapIfLarger(&a[j], &a[j+1]))
-      {
+      {        
+        #ifdef CBMC
+        __count_5_6++;
+        #endif
         swapped = 1;
       }
+      #ifdef CBMC
+      else __count_4_6++;
+      #endif
     }
     if (swapped == 0)
     {
+      #ifdef CBMC
+      __count_12_13++;
+      #endif
       break;
     }
+    #ifdef CBMC
+    else __count_8_9++;
+    #endif
   }
-  #ifdef CBMC
-  assert(__counti<=10);
-  assert(__countj<=10);
-  #endif
 }
 
 int
