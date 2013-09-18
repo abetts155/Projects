a0156d042df9695e4ffdbcd39587de9f2568c471
Paul Thomson <paul.thomson11@imperial.ac.uk>
2013-09-18 14:54:29 +0100

    Instrumented fibonacci.c

--- DaikonPathInformation/benchmarks/fibonacci.c
+++ DaikonPathInformation/benchmarks/fibonacci.c
@@ -2,16 +2,43 @@
 int 
 fibonacci (int n)
 {
-  if (n == 0) return 0;
-  if (n == 1) return 1;
+  #ifdef CBMC
+  int __count_2_9 = 0;
+  int __count_4_9 = 0;
+  int __count_7_6 = 0;
+  int __count_L7 = 0;
+  #endif
 
+  if (n == 0) // 1 
+  {
+    #ifdef CBMC
+    __count_2_9++;
+    #endif
+    return 0; // 2
+  }
+  if (n == 1) // 3
+  {
+    #ifdef CBMC
+    __count_4_9++;
+    #endif
+    return 1; // 4
+  }
+
+// 5
   int prevPrev = 0;
   int prev     = 1;
   int result   = 0;
   int i;
 
-  for (i = 2; i <= n; i++)
+  for (i = 2; 
+    #ifdef CBMC
+    __count_L7++,
+    #endif
+  i <= n; i++) // 7
   {
+    #ifdef CBMC
+    __count_7_6++;
+    #endif
     result   = prev + prevPrev;
     prevPrev = prev;
     prev     = result;
