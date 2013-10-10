9b97b38957bff724ba4ffb780d77a2fa1d0d7e30
Paul Thomson <paul.thomson11@imperial.ac.uk>
2013-10-10 11:04:25 +0100

    Reinstrumented matrix_inverse.c

--- DaikonPathInformation/benchmarks/matrix_inverse.c
+++ DaikonPathInformation/benchmarks/matrix_inverse.c
@@ -20,35 +20,47 @@ fabs (double n)
 int
 matrix_inverse (double a[][UPPERLIMIT], int row, int col, double eps)
 {
-  #ifdef CBMC
-  int __count_1_43 = 0;
-  int __count_2_43 = 0;
-  int __count_3_43 = 0;
-  int __count_5_6 = 0;
-  int __count_9_10 = 0;
-  int __count_9_11 = 0;
-  int __count_14_18 = 0;
-  int __count_17_16 = 0;
-  int __count_20_19 = 0;
-  int __count_22_30 = 0;
-  int __count_23_30 = 0;
-  int __count_25_26 = 0;
-  int __count_25_27 = 0;
-  int __count_38_37 = 0;
-  int __count_44_45 = 0;
+#ifdef CBMC
+//==========> matrix_inverse : header 38
+int __count_38_37 = 0; //Loop counter
+//==========> matrix_inverse : header 28
+int __count_25_26 = 0;
+int __count_25_27 = 0;
+int __count_28_25 = 0; //Loop counter
+//==========> matrix_inverse : header 35
+int __count_38_39 = 0;
+int __count_35_36 = 0; //Loop counter
+//==========> matrix_inverse : header 17
+int __count_17_16 = 0; //Loop counter
+//==========> matrix_inverse : header 31
+int __count_22_30 = 0;
+int __count_23_30 = 0;
+int __count_28_29 = 0;
+int __count_31_22 = 0; //Loop counter
+//==========> matrix_inverse : header 20
+int __count_20_19 = 0; //Loop counter
+//==========> matrix_inverse : header 12
+int __count_9_10 = 0;
+int __count_9_11 = 0;
+int __count_12_9 = 0; //Loop counter
+//==========> matrix_inverse : header 41
+int __count_35_40 = 0;
+int __count_41_35 = 0; //Loop counter
+//==========> matrix_inverse : header 33
+int __count_14_18 = 0;
+int __count_17_18 = 0;
+int __count_33_8 = 0; //Loop counter
+//==========> matrix_inverse : header 6
+int __count_6_5 = 0; //Loop counter
+//==========> matrix_inverse : header 1
+int __count_45 = 0;
+int __count_1_43 = 0;
+int __count_2_43 = 0;
+int __count_3_43 = 0;
+int __count_13_44 = 0;
+int __count_33_34 = 0;
 
-  int __count_L41 = 0;
-  int __count_L35 = 0;
-  int __count_L38 = 0;
-  int __count_L33 = 0;
-  int __count_L17 = 0;
-  int __count_L31 = 0;
-  int __count_L28 = 0;
-  int __count_L20 = 0;
-  int __count_L12 = 0;
-  int __count_L6 = 0;
-
-  #endif
+#endif
 
   int work[500];
   int i;
@@ -80,35 +92,41 @@ matrix_inverse (double a[][UPPERLIMIT], int row, int col, double eps)
     else if (eps <= 0.0)
       __count_3_43++;
     #endif
+    #ifdef CBMC
+    __count_45++;
+    #endif
     return;
   }
 
   w1 = 1.0;
-  for (i = 0; 
   #ifdef CBMC
-  __count_L6++,
+  __count_6_5 = 0;
   #endif
-  i < row; i++) // 6
+  for (i = 0; i < row; i++) // 6
   {
-    work[i] = i;
     #ifdef CBMC
-    __count_5_6++;
+    __count_6_5++;
     #endif
+    work[i] = i;
   }
 
-  for (k = 0; 
   #ifdef CBMC
-  __count_L33++,
+  __count_33_8 = 0;
   #endif
-  k < row; k++) // 33
+  for (k = 0; k < row; k++) // 33
   {
-    wmax = 0.0;
-    for (i = k; 
     #ifdef CBMC
-    __count_L12++,
+    __count_33_8++;
     #endif
-    i < row; i++) // 12
+    wmax = 0.0;
+    #ifdef CBMC
+    __count_12_9 = 0;
+    #endif
+    for (i = k; i < row; i++) // 12
     {
+      #ifdef CBMC
+      __count_12_9++;
+      #endif
       w = fabs (a[i][k]);
       if (w > wmax)
       {
@@ -129,7 +147,8 @@ matrix_inverse (double a[][UPPERLIMIT], int row, int col, double eps)
     {
       // 44
       #ifdef CBMC
-      __count_44_45++;
+      __count_13_44++;
+      __count_45++;
       #endif
       return;
     }
@@ -144,11 +163,10 @@ matrix_inverse (double a[][UPPERLIMIT], int row, int col, double eps)
       iw = work[k];
       work[k] = work[r];
       work[r] = iw;
-      for (j = 0; 
       #ifdef CBMC
-      __count_L17++,
+      __count_17_16 = 0;
       #endif
-      j < row; j++) // 17
+      for (j = 0; j < row; j++) // 17
       {
         #ifdef CBMC
         __count_17_16++;
@@ -159,16 +177,18 @@ matrix_inverse (double a[][UPPERLIMIT], int row, int col, double eps)
         a[k][j] = a[r][j];
         a[r][j] = w;
       }
+      #ifdef CBMC
+      __count_17_18++;
+      #endif
     }
     #ifdef CBMC
     else __count_14_18++;
     #endif
 
-    for (i = 0; 
     #ifdef CBMC
-    __count_L20++,
+    __count_20_19 = 0;
     #endif
-    i < row; i++) // 20
+    for (i = 0; i < row; i++) // 20
     {
       #ifdef CBMC
       __count_20_19++;
@@ -176,12 +196,14 @@ matrix_inverse (double a[][UPPERLIMIT], int row, int col, double eps)
       a[k][i] /= pivot;
     }
 
-    for (i = 0; 
     #ifdef CBMC
-    __count_L31++,
+    __count_31_22 = 0;
     #endif
-    i < row; i++) // 31
+    for (i = 0; i < row; i++) // 31
     {
+      #ifdef CBMC
+      __count_31_22++;
+      #endif
       if (i != k) // 22
       {
         v = i * col;
@@ -189,12 +211,14 @@ matrix_inverse (double a[][UPPERLIMIT], int row, int col, double eps)
         w = a[i][k];
         if (w != 0.0) // 23
         {
-          for (j = 0; 
           #ifdef CBMC
-          __count_L28++,
+          __count_28_25 = 0;
           #endif
-          j < row; j++) // 28
+          for (j = 0; j < row; j++) // 28
           {
+            #ifdef CBMC
+            __count_28_25++;
+            #endif
             if (j != k) // 25
             {
               #ifdef CBMC
@@ -206,6 +230,9 @@ matrix_inverse (double a[][UPPERLIMIT], int row, int col, double eps)
             else __count_25_27++;
             #endif
           }
+          #ifdef CBMC
+          __count_28_29++;
+          #endif
           a[i][k] = -w / pivot;
         }
         #ifdef CBMC
@@ -218,19 +245,26 @@ matrix_inverse (double a[][UPPERLIMIT], int row, int col, double eps)
     }
     a[k][k] = 1.0 / pivot;
   }
-
-  for (i = 0; 
   #ifdef CBMC
-  __count_L41++,
+  __count_33_34++;
   #endif
-  i < row; i++) // 41
+
+  #ifdef CBMC
+  __count_41_35 = 0;
+  #endif
+  for (i = 0; i < row; i++) // 41
   {
-    while (
     #ifdef CBMC
-    __count_L35++,
+    __count_41_35++;
     #endif
-    1) // 35
+    #ifdef CBMC
+    __count_35_36 = 0;
+    #endif
+    while (1) // 35
     {
+      #ifdef CBMC
+      __count_35_36++;
+      #endif
       k = work[i];
 
       if (k == i)
@@ -241,11 +275,10 @@ matrix_inverse (double a[][UPPERLIMIT], int row, int col, double eps)
       iw = work[k];
       work[k] = work[i];
       work[i] = iw;
-      for (j = 0; 
       #ifdef CBMC
-      __count_L38++,
+      __count_38_37 = 0;
       #endif
-      j < row; j++) // 38
+      for (j = 0; j < row; j++) // 38
       {
         #ifdef CBMC
         __count_38_37++;
@@ -257,8 +290,17 @@ matrix_inverse (double a[][UPPERLIMIT], int row, int col, double eps)
         a[k][i] = a[k][k];
         a[k][k] = w;
       }
+      #ifdef CBMC
+      __count_38_39++;
+      #endif
     }
+    #ifdef CBMC
+    __count_35_40++;
+    #endif
   }
+  #ifdef CBMC
+  __count_45++;
+  #endif
 }
 
 int
