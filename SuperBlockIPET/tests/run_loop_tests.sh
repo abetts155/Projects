5281ebdda0893d4385ab3c6c920fb981f94985a9
Adam Betts <a.betts@imperial.ac.uk>
2014-08-06 15:19:38 +0100

    Added scripts to repo which test super block Calculations

---
+++ SuperBlockIPET/tests/run_loop_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+trap ctrl_c INT
+
+function ctrl_c() 
+{
+  exit 0
+}
+
+for i in {20..20}
+do
+  echo "============================> SIZE $i"
+  python ../src/main_generate_program.py . --basic-blocks $i --unstructured --filename program$i.txt --loops 1 --subprograms 1
+  python ../src/main_super_blocks.py program$i.txt --repeat-calculation 10 --shuffle-constraints --use-ilp --use-tree-based
+done
