c77ab44b36879cfb27b590a71524145a29142507
Adam Betts <a.betts@imperial.ac.uk>
2014-08-11 16:20:11 +0100

    Removed redundant super block trees

--- SuperBlockIPET/tests/run_loop_tests.sh
+++ SuperBlockIPET/tests/run_loop_tests.sh
@@ -16,6 +16,6 @@ depth=${depth:=1}
 for i in $(seq $lower $upper)
 do
   echo "============================> SIZE $i"
-  python ../src/main_generate_program.py . --basic-blocks $i --filename program$i.txt --loops $loops --nesting-depth $depth --subprograms $subprograms
-  python ../src/main_super_blocks.py program$i.txt --use-ilp --use-tree-based
+  python ../src/main_generate_program.py . --unstructured --basic-blocks $i --filename program$i.txt --loops $loops --nesting-depth $depth --subprograms $subprograms
+  python ../src/main_super_blocks.py program$i.txt --use-tree-based
 done
--- SuperBlockTrees/src/edges.py
+++
@@ -1,9 +0,0 @@
-dummyID = 0
-
-class Edge ():    
-    def __init__ (self, vertexID, edgeID=None):        
-        self.vertexID = vertexID
-        if edgeID is None:
-            self.edgeID = dummyID
-        else:
-            self.edgeID = edgeID
