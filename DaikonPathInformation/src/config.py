1f2737333519af58a818b489380fe1a298cee7f3
abetts155 <adam.betts155@gmail.com>
2020-08-12 15:24:32 +0100

    Tidy up

--- DaikonPathInformation/src/config.py
+++
@@ -1,9 +0,0 @@
-
-class Arguments:
-    """Globals and arguments passed on the command line"""
-    
-    m5_trace_directory = "m5out"
-    GCC                = "arm-linux-gnueabi-gcc"
-    objdump            = "arm-linux-gnueabi-objdump"
-    basename           = None
-    basepath           = None
